f69ebda6d197b84d8766c6474e955c8e346d1fb3
clarice <claricealves330@gmail.com>
2023-11-17 09:48:02 -0300

    :tada: first commit :tada:

---
+++ src/app/pages/pages.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+// Module Routing
+import { RoutingModule } from './routing.modules';
+
+// Module
+import { SharedModule } from '../shared/shared.module';
+
+// Pages 
+import { HomeComponent } from './home/home.component';
+import { DetailsComponent } from './details/details.component';
+
+@NgModule({
+  declarations: [
+    HomeComponent,
+    DetailsComponent
+  ],
+  imports: [
+    CommonModule,
+    RoutingModule,
+    SharedModule
+  ]
+})
+export class PagesModule { }
---
+++ src/app/shared/poke-header/poke-header.component.ts
@@ -0,0 +1,10 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'poke-header',
+  templateUrl: './poke-header.component.html',
+  styleUrls: ['./poke-header.component.scss']
+})
+export class PokeHeaderComponent{
+  
+}
